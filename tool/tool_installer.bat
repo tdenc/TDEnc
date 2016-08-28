d381588e21e23034bea4fb841158deb905dd2dcc
tdenc <tdenc1125@gmail.com>
2016-08-28 12:07:58 +0900

    Revert "v2.81"
    This reverts commit b6569e685de480c09836ea133e39e3c39cddb341.

--- tool/tool_installer.bat
+++ tool/tool_installer.bat
@@ -18,7 +18,7 @@ echo;
 
 .\7z.exe e -bd -y %DSS_PATH% "DirectShowSource.dll"
 .\7z.exe e -bd -y %DIL_PATH% "DevIL.dll"
-.\7z.exe e -bd -y %FSS_PATH% "*\x86\ffms2.dll" "*\x86\ffmsindex.exe"
+.\7z.exe e -bd -y %FSS_PATH% "*\ffms2.dll" "*\ffmsindex.exe"
 .\7z.exe e -bd -y %RG1_PATH% "RemoveGrain.dll" "Repair.dll"
 .\7z.exe e -bd -y %QTS_PATH% "QTSource.dll"
 .\7z.exe e -bd -y %MIF_PATH% "MediaInfo.exe" "MediaInfo.dll"
